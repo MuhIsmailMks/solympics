d73c298b0c62f7e03304a12565c4b13f3891c930
muhammad ismail <muhismailmks@gmail.com>
2024-07-13 16:07:07 +0800

    ad

--- static/js/index.js
+++ static/js/index.js
@@ -13,46 +13,49 @@ closeBtn.addEventListener('click', () =>{
 
 
 // animation scroll effect
+// controller ScrollMagic
+let controller = new ScrollMagic.Controller();
 const animations = [
-    { selector: ".how_to1", duration: 7000, x: 100 },  
+    { selector: ".slideshow_container", duration: 2000, x: 200 },  
+    { selector: ".syrenge", duration: 4000, x: -100 },  
   ];
   
   
-// function adjustXValue() {
-//     const screenWidth = window.innerWidth;
-//     if (screenWidth > 1200) { 
-//       animations.forEach(animation => {
-//         if (animation.hasOwnProperty('x')) {  
-//           animation.x = animation.x * 4;
-//         }
-//         if (animation.hasOwnProperty('y')) { 
-//           animation.y = animation.y * 2;
-//         } 
-//       });
-//     }
-// }
+function adjustXValue() {
+    const screenWidth = window.innerWidth;
+    if (screenWidth > 1200) { 
+      animations.forEach(animation => {
+        if (animation.hasOwnProperty('x')) {  
+          animation.x = animation.x * 4;
+        }
+        if (animation.hasOwnProperty('y')) { 
+          animation.y = animation.y * 2;
+        } 
+      });
+    }
+}
   
-//   adjustXValue();
-//   window.addEventListener('resize', adjustXValue);
+  adjustXValue();
+  window.addEventListener('resize', adjustXValue);
   
-//   animations.forEach(animation => { 
-//     var tweenParams = { duration: 300, x: animation.x };
-//     if (animation.hasOwnProperty('x')) {
-//       tweenParams.x = animation.x;
-//     }
-//     if (animation.hasOwnProperty('y')) {
-//       tweenParams.y = animation.y;
-//     } 
+  animations.forEach(animation => { 
+    var tweenParams = { duration: 300, x: animation.x };
+    if (animation.hasOwnProperty('x')) {
+      tweenParams.x = animation.x;
+    }
+    if (animation.hasOwnProperty('y')) {
+      tweenParams.y = animation.y;
+    } 
     
-//     var tween = gsap.to(animation.selector, tweenParams);
+    var tween = gsap.to(animation.selector, tweenParams);
   
-//     var scene = new ScrollMagic.Scene({
-//       triggerElement: animation.selector,
-//       duration: animation.duration
-//     })
-//     .setTween(tween)
-//     .addTo(controller);
-//   });
+    var scene = new ScrollMagic.Scene({
+      triggerElement: animation.selector,
+      duration: animation.duration
+    })
+    .setTween(tween)
+    .addTo(controller);
+  });
   
   
   
